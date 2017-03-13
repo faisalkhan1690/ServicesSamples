62d07059b0f76e3f2ac1e9a2d86901ef340c5093
Faisal Khan <faisalkhan1690@gmail.com>
2017-03-14 00:08:13 +0530

    + new phase started

---
+++ app/src/main/java/com/example/faisal/servicessamples/ForegroundServiceExample.java
@@ -0,0 +1,13 @@
+package com.example.faisal.servicessamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForegroundServiceExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foreground_service_example);
+    }
+}
--- app/src/main/java/com/example/faisal/servicessamples/MainActivity.java
+++ app/src/main/java/com/example/faisal/servicessamples/MainActivity.java
@@ -55,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case 3:
+                        startActivity(new Intent(MainActivity.this,ForegroundServiceExample.class));
+                        break;
+
+                    case 4:
                         startActivity(new Intent(MainActivity.this,JobSchedulerActivity.class));
                         break;
 
